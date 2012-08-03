a1593d6670c3e21d5179411ecdcf118753fdd183
Yaraslau Kurmyza <lotask@gmail.com>
2012-08-03 08:43:22 +0300

    switched to auto-discovery of remote 'origin' path, instead of hadrcoded one

--- GotoCommit/gotocommit.py
+++ GotoCommit/gotocommit.py
@@ -5,6 +5,7 @@ import subprocess
 import re
 import time
 
+
 git_root_cache = {}
 def git_root(directory):
     global git_root_cache
@@ -26,17 +27,21 @@ def git_root(directory):
             break
         directory = parent
 
-    git_root_cache[leaf_dir] = { 'retval': retval, 'expires': time.time() + 5 }
+    git_root_cache[leaf_dir] = {'retval': retval, 'expires': time.time() + 5}
 
     return retval
 
+
 class GotoCommitCommand(sublime_plugin.TextCommand):
-    stdin = False
+    remote = "origin"
 
     def run(self, edit):
+        if not self.view.file_name():
+            return
+
         command = ['git', 'blame', '-w', '-M', '-C']
 
-        selection = self.view.sel()[0]  #first line
+        selection = self.view.sel()[0]  # first line
 
         #only first line
         beginLine, beginCol = self.view.rowcol(selection.begin())
@@ -54,17 +59,36 @@ class GotoCommitCommand(sublime_plugin.TextCommand):
 
         from subprocess import Popen, PIPE, STDOUT
 
-        proc = Popen(command, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
-        # proc = subprocess.Popen(command)
-        output = proc.communicate()[0]
-        print  output + "\n"
+        output = self.getCommandOutput(command)
 
         sha = re.split('\s', output)[0]
 
-        commitUrl = "https://github.com/yangutu/Yangutu/commit/%s" % sha
+        commitUrl = self.findGithubUrl() + "commit/%s" % sha
 
         #inform
-        sublime.status_message(output)
+        sublime.status_message(commitUrl)
 
         self.view.window().run_command('open_url', {"url": commitUrl})
 
+    def findGithubUrl(self):
+        command = ['git', 'remote', '-v']
+
+        remotes = self.getCommandOutput(command)
+
+        regex = r'origin\s*(?:https://github\.com/|git@github\.com:)(.*)/(.*?)(?:\.git)'
+
+        result = re.search(regex, remotes)
+        if not result:
+            sublime.status_message('Could not find origin remote')
+            return ''
+
+        matches = result.groups()
+
+        full_link = 'https://github.com/%s/%s/' % (matches[0], matches[1])
+        return full_link
+
+    def getCommandOutput(self, command):
+        from subprocess import Popen, PIPE, STDOUT
+
+        proc = Popen(command, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        return proc.communicate()[0]
